21bf461378680d8261e79193ec27c5e1c037e266
IgorFratczak <245804@edu.p.lodz.pl>
2024-06-21 18:49:45 +0200

    7. Dodanie final do stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,9 +1,9 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
-    private int SIZE = 12;
-    private int[] NUMBERS = new int[SIZE];
-    private int CHECK = -1;
+    final private int SIZE = 12;
+    final private int[] NUMBERS = new int[SIZE];
+    final private int CHECK = -1;
     public int total = -1;
 
     public void countIn(int in) {
