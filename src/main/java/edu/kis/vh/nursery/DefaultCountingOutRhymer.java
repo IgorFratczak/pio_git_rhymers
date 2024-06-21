2c404856a1d6df40ea8d47a7608829f4c9dad2b4
IgorFratczak <245804@edu.p.lodz.pl>
2024-06-21 18:49:13 +0200

    6. Tworzenie Stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,34 +1,34 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
-
-    private int[] numbers = new int[12];
-
+    private int SIZE = 12;
+    private int[] NUMBERS = new int[SIZE];
+    private int CHECK = -1;
     public int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
-            numbers[++total] = in;
+            NUMBERS[++total] = in;
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == CHECK;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == SIZE-1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
-        return numbers[total];
+            return CHECK;
+        return NUMBERS[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
-        return numbers[total--];
+            return CHECK;
+        return NUMBERS[total--];
     }
 
 }
